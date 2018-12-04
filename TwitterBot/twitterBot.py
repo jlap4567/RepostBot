a9e6494cdc32760eeb8a0451154883118251d1f6
Jacob Lapinson <35976327+jlap4567@users.noreply.github.com>
2018-12-04 11:52:00 -0500

    The Twitterbot code
    This is the current state of the code upon creating the repository. The code it throwing an error code 400 which I am attempting to resolve

---
+++ TwitterBot/twitterBot.py
@@ -0,0 +1,148 @@
+"""
+Writen by: Jacob Lapinson
+Date: 12/01/18
+
+This program tells a twitter account to check the most popular
+tweets using the Floridaman search parameter then selects the most
+popular one that has not been retweeted by the account and retweets it
+"""
+import tweepy #allows the program to interact with twitter api
+import csv #allows program to create a csv file to use as a database
+import datetime #allow program to get the current date and time
+import os #allows program to operate using mac os commands
+import time #allows program to wait for an amount of time
+import threading #allows the program to run multithreaded
+
+#Keys to access twitter account
+CONSUMER_KEY = 'bz5MIgTVltSAGrOVRwTEBJFgm'
+CONSUMER_SECRET = 'sfMSclxsco6A34DqPEUok2hAifIzhZs7mrPGOcYvFNRKreZuHf'
+ACCESS_KEY = '1061740751288262658-PHkUt0Hs3myAj1cCmsvWRmSu2THOR5'
+ACCESS_SECRET = 'DO5Dr8L3UWGI7IelaByq78tjC9ANcSDXttZmlGGl0EUyc'
+
+#Uses twitter keys
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#Makes sure the program doesnt try to retweet the same thing
+usedTweets = []
+
+#sets up some variables (wait could be done in a function but I it also
+# works putting here because it is easier to change)
+search = ' '
+wait = 60
+
+def getFMPosts():
+    """
+    Collects 100 posts with Floridaman Hashtag from today
+    and saves them in a csv file
+    search - string: the key word or key phrase you want to search for
+    """
+    #Gets and formats todays date
+    now = datetime.datetime.now()
+    date = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+    # Open/Create a file to append data
+    csvFile = open('FM.csv', 'a')
+    #Use csv Writer
+    csvWriter = csv.writer(csvFile)
+
+    #creates a csv file that contains the last 100 post that have been posted
+    #in that day
+    for tweet in tweepy.Cursor(api.search,q=search,count=1000,
+                               lang="en",
+                               since=date).items():
+        csvWriter.writerow([tweet.id, tweet.favorite_count])
+
+
+def postPicker():
+    """
+    Selects which post to pick from a list of the 100 most resent posts with
+    the Hashtag.
+    usedPosts - array of post objects which contains all of the
+        already posted posts
+    """
+    maxLikes = 0
+    topPost = 'NULL'
+
+    #Opens csv file that contains all recent tweets
+    csvFile = open('FM.csv', 'r')
+    #Creates a csv reader that can iterate through the file
+    tweetReader = csv.reader(csvFile, delimiter=' ', quotechar='\n')
+    #Iterates through csv file to find the most popular tweet
+    for tweet in tweetReader:
+        stweet = tweet[0].split(",")
+        if(int(stweet[1]) >= maxLikes):
+            if(stweet[0] not in usedTweets):
+                maxLikes = int(stweet[1])
+                topPost = stweet[0]
+
+    usedTweets.append(topPost)
+    return topPost
+
+
+def repostTweet():
+    """
+    Allows a thread to continuously retweet posts that have the key
+    phrase that matches search
+    PostID - int, the id of the post that is going to be retweeted
+    """
+    while(True):
+        while(True):
+            getFMPosts()
+            postID = postPicker()
+
+            try:
+                api.retweet(postID)
+            except tweepy.error.TweepError:
+                break
+
+            os.remove('FM.csv')
+            print("Repost Made")
+            time.sleep(wait*60)
+
+
+def recentMention():
+    """
+    Returns the most recent mention to your account
+    """
+    newMention = api.mentions_timeline(count = 1) #for some reason my compiler is fucking up here
+    return newMention[0].text
+
+
+def mentionChecker(mention):
+    """
+    This funciton checks to see if the newist mention is already in use
+    and returns the key phrase that is in the tweet
+    mention - string, the text of the latest mention
+    """
+    smention = mention.split(" ")
+    if(" ".join(smention[1:]) == search):
+        return None
+    else:
+        return " ".join(smention[1:])
+
+def getNewMention():
+    """
+    A funciton that allows a thread to continuously check for new mentions
+    """
+    while(True):
+        if(mentionChecker(recentMention()) != None):
+            search = mentionChecker(recentMention())
+            print("looking for key phrase: " + search)
+            time.sleep(wait*60)
+
+
+def main():
+    #thread that looks for the newist mention
+    t1 = threading.Thread(target=getNewMention, args=[])
+    #thread that retweets the keyword
+    t2 = threading.Thread(target=repostTweet, args=[])
+
+    #Runs the threads
+    t1.start()
+    time.sleep(10)
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
